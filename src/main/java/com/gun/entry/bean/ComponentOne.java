12ea5301a0818abd2e5639a83abb0cd2a035c312
eku-code <kuzhabaev.zhenya@mail.ru>
2020-01-26 21:47:53 +0300

    dockerize redis

--- src/main/java/com/gun/entry/bean/ComponentOne.java
+++
@@ -1,7 +0,0 @@
-package com.gun.entry.bean;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ComponentOne {
-}
--- src/main/java/com/gun/entry/configuration/Configuration.java
+++ src/main/java/com/gun/entry/configuration/Configuration.java
@@ -1,7 +1,9 @@
 package com.gun.entry.configuration;
 
 import com.gun.entry.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -10,9 +12,19 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class Configuration {
 
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
